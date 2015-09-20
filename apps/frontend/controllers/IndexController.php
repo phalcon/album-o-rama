7bad847c2dc98d0e47a06aa5a3241f5bc0e6d1c7
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-20 17:50:10 +0300

    Cleaned index controller

--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -10,43 +10,41 @@ class IndexController extends ControllerBase
         $key = 'index'.$offset;
 
         $exists = $this->view->getCache()->exists($key);
-        if (!$exists) {
 
-            //Top albums
-            $phql = 'SELECT
-            al.id,
-            al.name,
-            ar.uri,
-            ar.id as artist_id,
-            ar.name as artist,
-            ap.url
-            FROM AlbumOrama\Models\Albums al
-            JOIN AlbumOrama\Models\Artists ar
-            JOIN AlbumOrama\Models\AlbumsPhotos ap
-            WHERE
-            al.id >= '.($offset * 30).' AND
-            ap.type = "large"
-            ORDER BY al.playcount DESC
-            LIMIT 30';
+        if (!$exists) {
+            // Top albums
+            $phql = '
+                SELECT
+                    al.id, al.name, ar.uri, ar.id as artist_id, ar.name as artist, ap.url
+                FROM AlbumOrama\Models\Albums al
+                JOIN AlbumOrama\Models\Artists ar
+                JOIN AlbumOrama\Models\AlbumsPhotos ap
+                WHERE
+                    al.id >= '.($offset * 30).' AND ap.type = "large"
+                ORDER BY al.playcount DESC
+                LIMIT 30';
+
             $albums = $this->modelsManager->executeQuery($phql);
 
             $this->view->setVar('albums', $albums);
 
-            //Top tags
-            $phql = 'SELECT t.name, COUNT(*)
-            FROM AlbumOrama\Models\Tags t
-            JOIN AlbumOrama\Models\AlbumsTags at
-            GROUP BY 1
-            HAVING COUNT(*) > 50
-            LIMIT 10
-            OFFSET '.mt_rand(0, 50);
+            // Top tags
+            $phql = '
+                SELECT
+                    t.name, COUNT(*)
+                FROM AlbumOrama\Models\Tags t
+                JOIN AlbumOrama\Models\AlbumsTags at
+                GROUP BY 1
+                HAVING COUNT(*) > 50
+                LIMIT 10
+                OFFSET '.mt_rand(0, 50);
+
             $tags = $this->modelsManager->executeQuery($phql);
 
             $this->view->setVar('tags', $tags);
-
         }
 
-        $this->view->cache(array("key" => $key));
+        $this->view->cache(['key' => $key]);
     }
 
     public function route404Action()
